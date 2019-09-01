410ce267a6f8c03e7216118562a17d8faa8907a1
bruce_s <354416454@wsy>
2019-09-01 22:41:57 +0800

    Default Changelist

--- src/test/java/com/ourdream/TestVariable.java
+++ src/test/java/com/ourdream/TestVariable.java
@@ -1,6 +1,30 @@
 package com.ourdream;
 
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.repository.Deployment;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MyApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TestVariable {
 
+    /**
+     * 部署
+     */
+    @Test
+    public void deploy(){
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
 
+        Deployment deployment = processEngine.getRepositoryService()
+                .createDeployment()
+                .addClasspathResource("processes/leave.bpmn")
+                .deploy();
+        //.addClasspathResource("processes/iot_order.png")
+
+        System.out.println(deployment.getId()+"          " + deployment.getName() + "          " + deployment.getKey());//12505          null
+    }
 }
